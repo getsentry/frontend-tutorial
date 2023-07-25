c60690688130fe8055e46806ba2db766c87f275c
vivianyentran <vivian.tran@sentry.io>
2023-07-25 11:17:20 -0700

    Added a new error and some other stuff

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from "./components/App";
 import * as Sentry from "@sentry/react";
 
 Sentry.init({
-  dsn: "https://d961495f73d54f6b908522b6be60b106@o4505201972871168.ingest.sentry.io/4505511974207488",
+  dsn: "https://2f4e649d19e64637903b24e784c7a4f2@o4505201972871168.ingest.sentry.io/4505523376553985",
   integrations: [new Sentry.BrowserTracing({}), new Sentry.Replay()],
 
   // Set tracesSampleRate to 1.0 to capture 100%

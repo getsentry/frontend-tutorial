6c63e0339c5210bca669f309a66fc6621b45e03a
vivianyentran <vivian.tran@sentry.io>
2023-07-25 11:55:39 -0700

    change error

--- src/components/App.js
+++ src/components/App.js
@@ -94,7 +94,7 @@ class App extends Component {
 
   checkout() {
     // Generate an error
-    this.myCodeIsEvenMorePerfect();
+    this.myCodeIsMorePerfect();
 
     const order = {
       email: this.email,

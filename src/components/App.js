5da1ff1723a3967f7857a53b202b203b1349f5f5
Dhrumil <dhrumil.parekh@sentry.io>
2023-09-03 14:55:04 -0700

    test1

--- src/components/App.js
+++ src/components/App.js
@@ -94,7 +94,7 @@ class App extends Component {
 
   checkout() {
     // Generate an error
-    // this.myCodeIsPerfect();
+    this.myCodeIsNot200Perfect();
 
     const order = {
       email: this.email,

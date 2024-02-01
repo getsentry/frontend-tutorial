ce68e195fa626706a710d200dda3a14e7fdb8614
vivianyentran <vivian.tran@sentry.io>
2024-02-01 14:01:41 -0800

    test tutorial

--- src/components/App.js
+++ src/components/App.js
@@ -94,7 +94,7 @@ class App extends Component {
 
   checkout() {
     // Generate an error
-    // this.myCodeIsPerfect();
+    this.myCodeIsMorePerfect();
 
     const order = {
       email: this.email,
--- src/index.js
+++ src/index.js
@@ -3,6 +3,21 @@ import { createRoot } from "react-dom/client";
 import App from "./components/App";
 
 // Import and init Sentry SDK
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://2f4e649d19e64637903b24e784c7a4f2@o4505201972871168.ingest.sentry.io/4505523376553985",
+  integrations: [new Sentry.BrowserTracing({}), new Sentry.Replay()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  tracesSampleRate: 1.0,
+
+  // Capture Replay for 10% of all sessions,
+  // plus for 100% of sessions with an error
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
 
 const container = document.getElementById("root");
 const root = createRoot(container);
